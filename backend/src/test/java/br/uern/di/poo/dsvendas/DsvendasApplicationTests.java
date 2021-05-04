d3b51403b217ad78443515fcc191cf6f3ef3ce3b
Sebastião Emidio Alves Filho <sebastiao.alves@gmail.com>
2021-05-03 23:24:20 -0300

    Project created

---
+++ backend/src/test/java/br/uern/di/poo/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uern.di.poo.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
