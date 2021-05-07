4c97cd60a746472548cafa7df792f26cd8fa88e8
Sebastião Emidio Alves Filho <sebastiao.alves@gmail.com>
2021-05-07 20:33:29 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -10,23 +10,19 @@ type ChartData = {
 };
 
 const DonutChart = () => {
-
-  const [chartData, setChartData] = useState<ChartData> ({ labels: [], series: [] });
-
-  useEffect ( () => {
-    axios.get(`${BASE_URL}/sales/amount-by-seller/`)
-    .then((response) => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => x.sum);
-      setChartData({labels: myLabels, series: mySeries});
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: [],
   });
-  }, []);
 
-  //    const mockData = {
-  //        series: [477138, 499928, 444867, 220426, 473088],
-  //        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  //    }
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller/`).then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
+      setChartData({ labels: myLabels, series: mySeries });
+    });
+  }, []);
 
   const options = {
     legend: {
